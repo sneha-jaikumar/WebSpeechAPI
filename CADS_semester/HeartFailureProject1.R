942311f4b640a809ac855047f00f22c3fe37fe4a
Sneha Jaikumar <snehaj@email.unc.edu>
2021-04-18 15:17:48 -0400

    Added plot descriptions paragraph to readme

---
+++ CADS_semester/HeartFailureProject1.R
@@ -0,0 +1,10 @@
+# The Cool Data Scientists
+# Heart Disease Dataset
+# Sneha Jaikumar
+
+install.packages("tidyverse")
+library(tidyverse)
+library(readr)
+
+heartFailure <- read_csv("heart_failure_clinical_records_dataset.csv")
+
